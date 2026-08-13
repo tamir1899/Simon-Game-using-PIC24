b324a7683983744c07d959ea37294e876ac3953f
tamir1899 <tamirhussein93@gmail.com>
2026-08-12 21:39:28 -0500

    Create simon.h

---
+++ src/simon.h
@@ -0,0 +1,12 @@
+#ifndef SIMON_H
+#define SIMON_H
+
+#include <xc.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+void simon_init(void);
+void simon_play_game(void);
+void play_mario_intro(void);
+
+#endif
