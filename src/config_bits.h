fb8039d085671fad52303962e2e17e0157c81861
tamir1899 <tamirhussein93@gmail.com>
2026-08-12 21:40:39 -0500

    Create config_bits.h

---
+++ src/config_bits.h
@@ -0,0 +1,20 @@
+#ifndef CONFIG_BITS_H
+#define CONFIG_BITS_H
+
+#include <xc.h>
+
+#pragma config ICS = PGx1
+#pragma config FWDTEN = OFF
+#pragma config GWRP = OFF
+#pragma config GCP = OFF
+#pragma config JTAGEN = OFF
+
+#pragma config I2C1SEL = PRI
+#pragma config IOL1WAY = OFF
+#pragma config OSCIOFNC = ON
+#pragma config FCKSM = CSECMD
+#pragma config FNOSC = FRC
+#pragma config IESO = OFF
+#pragma config POSCMOD = NONE
+
+#endif
